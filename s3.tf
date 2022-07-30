fac03d8cb8de85598d2b4c5e2f7493ca9388c432
ingoaf <ilija02@web.de>
2022-07-30 14:31:39 +0200

    fix versioning

--- s3.tf
+++ s3.tf
@@ -14,20 +14,23 @@ resource "aws_s3_bucket_public_access_block" "terraform-demo" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
-  versioning {
-    enabled = true
-  }
 }
 
 # Optional: encrypt the bucket
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-demo" {
   bucket = aws_s3_bucket.terraform-demo.bucket
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-demo" {
+  bucket = aws_s3_bucket.terraform-demo.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
